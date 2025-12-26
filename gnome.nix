39783d4fca1e8923c9e960d7d1d5a44de02b1701
tuhana <tuhana.cat+git@gmail.com>
2025-12-26 13:39:11 +0300

    Enable core GNOME apps and experimental scaling options

--- gnome.nix
+++ gnome.nix
@@ -1,20 +1,39 @@
 { pkgs, ... }:
-let
-  nixos-background-info = pkgs.stdenv.mkDerivation { name = "nixos-background-info"; };
-in
 {
   services = {
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
 
     gnome = {
-      core-apps.enable = false;
+      core-apps.enable = true;
       core-developer-tools.enable = false;
       games.enable = false;
     };
 
     flatpak.enable = true;
   };
-
-  environment.gnome.excludePackages = [ nixos-background-info ];
+  
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    simple-scan
+    gnome-tour
+    gnome-connections
+    geary
+  ];
+  
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/mutter" = {
+          experimental-features = [
+            "scale-monitor-framebuffer"
+            "xwayland-native-scaling"
+          ];
+        };
+      };
+    }
+  ];
 }
