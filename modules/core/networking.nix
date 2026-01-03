935237a449f3320fe6efd5cc75de83d3a0348461
tuhana <tuhana.cat+git@gmail.com>
2026-01-03 16:58:12 +0300

    Enable systemd-resolved

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -2,9 +2,20 @@
 {
   networking = {
     nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
     ];
+
     timeServers = [ "time.cloudflare.com" ];
+  };
+
+  services.resolved = {
+    enable = true;
+
+    dnssec = "true";
+    dnsovertls = "true";
+
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
   };
 }
