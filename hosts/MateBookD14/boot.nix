94154c894cf7c1f4ed3c19148dccca0f6c087248
tuhana <tuhana.cat+git@gmail.com>
2026-01-08 21:39:35 +0300

    Enable silent boot

--- hosts/MateBookD14/boot.nix
+++ hosts/MateBookD14/boot.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }:
 {
   boot = {
+    # TODO: Maybe drop this, let the kernel
+    # decide which one to load?
     kernelParams = [
+      # Force load the Xe driver
       "i915.force_probe=!46a6"
       "xe.force_probe=46a6"
+      # Silent boot
+      "quiet"
+      "udev.log_level=3"
     ];
+
+    initrd.verbose = false;
+    consoleLogLevel = "0";
 
     plymouth.enable = true;
   };
