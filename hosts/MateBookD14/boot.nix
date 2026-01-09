b8f30c9b480ec859c511a47463191a2bde518555
tuhana <tuhana.cat+git@gmail.com>
2026-01-09 11:47:39 +0300

    Let the kernel decide which Intel GPU driver to load

--- hosts/MateBookD14/boot.nix
+++ hosts/MateBookD14/boot.nix
@@ -1,13 +1,7 @@
 { pkgs, ... }:
 {
   boot = {
-    # TODO: Maybe drop this, let the kernel
-    # decide which one to load?
     kernelParams = [
-      # Force load the Xe driver
-      "i915.force_probe=!46a6"
-      "xe.force_probe=46a6"
-      # Silent boot
       "quiet"
       "udev.log_level=3"
     ];
