0a7daf8f581f2fc1744426fb5b59ecc077fc0a2b
tuhana <tuhana.cat+git@gmail.com>
2026-01-13 13:38:36 +0300

    Remove 32 bit packages

--- hosts/MateBookD14/hardware.nix
+++ hosts/MateBookD14/hardware.nix
@@ -12,10 +12,7 @@
     cpu.intel.updateMicrocode = true;
 
     graphics = {
-      enable32Bit = true;
-
       extraPackages = [ pkgs.intel-media-driver ];
-      extraPackages32 = [ pkgs.pkgsi686Linux.intel-media-driver ];
     };
   };
 }
