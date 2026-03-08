213e02186179cc693afd9792c6f835eb9f4f3842
tuhana <tuhana.cat+git@gmail.com>
2026-03-08 22:53:23 +0300

    Add authorised OpenSSH keys

--- users/tuhana.nix
+++ users/tuhana.nix
@@ -8,6 +8,11 @@ in
 
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINRxlolhp8bTNWcjkPz/Ib3jeru3r3URp3QGAY/meoww meow"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKYu6luGMLWJ66nJxf/UrS18bHs2ru9rOzVaOKk+7THCdacTcq8IrEuvF8L0snXiCc9OtxvkUr05sN448cub2sw="
+    ];
   };
 
   home-manager.users.tuhana = {
