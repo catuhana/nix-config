4937af26d4750004f8b2ea13b8089e6db92ec164
tuhana <tuhana.cat+git@gmail.com>
2026-04-06 20:05:25 +0300

    Add Copyous GNOME extension, update lockfile

--- modules/aspects/gnome/shell/extensions.nix
+++ modules/aspects/gnome/shell/extensions.nix
@@ -11,6 +11,7 @@
           <caden/gnome/shell/extensions/blur-my-shell>
           <caden/gnome/shell/extensions/caffeine>
           <caden/gnome/shell/extensions/appindicator>
+          <caden/gnome/shell/extensions/copyous>
         ];
 
         provides.blur-my-shell = {
@@ -39,6 +40,25 @@
             {
               programs.gnome-shell.extensions = [
                 { package = pkgs.gnomeExtensions.appindicator; }
+              ];
+            };
+        };
+
+        provides.copyous = {
+          homeManager =
+            { pkgs, ... }:
+            {
+              programs.gnome-shell.extensions = [
+                {
+                  package = pkgs.gnomeExtensions.copyous.overrideAttrs (old: {
+                    buildInputs = [ pkgs.libgda5 ];
+
+                    preInstall = ''
+                      sed -i "1i import GIRepository from 'gi://GIRepository';\nGIRepository.Repository.dup_default().prepend_search_path('${pkgs.libgda5}/lib/girepository-1.0');\nGIRepository.Repository.dup_default().prepend_search_path('${pkgs.gsound}/lib/girepository-1.0');\n" lib/preferences/dependencies/dependencies.js
+                      sed -i "1i import GIRepository from 'gi://GIRepository';\nGIRepository.Repository.dup_default().prepend_search_path('${pkgs.libgda5}/lib/girepository-1.0');\n" lib/misc/db.js
+                    '';
+                  });
+                }
               ];
             };
         };
