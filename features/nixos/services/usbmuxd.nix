fc9bbf9cbfc28febb1121a607ec8db88d28b990c
tuhana <tuhana.cat+git@gmail.com>
2026-05-01 18:34:07 +0300

    Replace usbmuxd with usbmuxd2
    In hopes of fixing timing outs and it getting stuck.

--- features/nixos/services/usbmuxd.nix
+++ features/nixos/services/usbmuxd.nix
@@ -6,6 +6,8 @@ _: {
         {
           services.usbmuxd = {
             enable = true;
+
+            package = pkgs.usbmuxd2;
           };
 
           environment.systemPackages = [ pkgs.libimobiledevice ];
